190e0d20a072f60f25b491efa8aa8cfb510aac82
Bart <bartvd123@live.nl>
2025-04-21 18:43:21 +0200

    Add hints for each location on test page

---
+++ hints.js
@@ -0,0 +1,11 @@
+const hints = new Map();
+
+hints.set('51.51916;4.68535', 'In het midden van het doolhof. Volwassenen moeten bukken.')
+hints.set('51.51904;4.68503', 'Tegen het raam')
+hints.set('51.51828651412727;4.683997648875061','Wat zit er aan de prullenbak dat er niet hoort?')
+hints.set('51.51844;4.68614','Achterkant petpot')
+hints.set('51.51759222154679;4.6878986153795275','Onderzoek het zwarte paaltje (de QR code zit goed verborgen)')
+hints.set('51.51848564860585;4.687249856074049','Tegen de trap')
+hints.set('51.51865508877846;4.686387106780603','Onder het bankje')
+hints.set('51.51961008091631;4.686148054902151','In het stopcontact')
+hints.set('51.52025;4.68485','De schat is begraven achter een bosje. Op de kist ligt een tak met schroeven erin.')
